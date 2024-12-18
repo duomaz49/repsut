e7aee4c7abb5b756e0617513893915dccf4aa205
Tomaatti <tuomas49@gmail.com>
2024-12-18 11:17:27 +0200

    Fixed eslint errors

--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
 import Category from './category';
